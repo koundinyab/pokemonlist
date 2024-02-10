99a182d0317fd627ce191628f3e8e22650b8c106
koundinya_b <koundinya.001@gmail.com>
2024-02-10 14:33:27 -0600

    pokemon list with load more

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [pmList, setPmList] = useState([]);
+    const [displayList, setDisplayList] = useState([]);
+    const [pageNumber, setPageNumber] = useState(0);
+    const [disableButton, setDisableButton] = useState(false);
+    useEffect(() => {
+        const getPmList = async () => {
+            const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+            const list = await response.json();
+            setPmList(list.results);
+        };
+        getPmList();
+    }, []);
+    useEffect(() => {
+        if (pmList.length > 0) {
+            setDisplayList(pmList.slice(0, (pageNumber + 1) * 5));
+            if ((pageNumber + 1) * 5 >= pmList.length) {
+                setDisableButton(true);
+            }
+        }
+    }, [pageNumber, pmList]);
+    function handleLoadMore() {
+        setPageNumber((prev) => prev + 1);
+    }
+    return (
+        <>
+            <h1>Pokemon list</h1>
+            <ul>
+                {displayList.map((pokemon) => {
+                    return (
+                        <li key={pokemon.url + pokemon.name}>{pokemon.name}</li>
+                    );
+                })}
+            </ul>
+            <button onClick={handleLoadMore} disabled={disableButton}>
+                Load more
+            </button>
+        </>
+    );
 }
 
 export default App;
